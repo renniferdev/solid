4ce758407892e6b28a0c52d742eeb847d6e01fe2
Rennifer Amilton Teixeira <rennifer1-@gmail.com>
2024-08-05 21:41:02 -0300

    configurações

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+	env: {
+		node: true,
+		browser: true,
+		es6: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier/@typescript-eslint',
+	],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+	rules: {
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/ban-ts-ignore': 'off',
+		'@typescript-eslint/camelcase': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/member-delimiter-style': 'warn',
+	}
+};
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true, //habilita ' aspas simples
+    printWidth: 80, //largura
+    tabWidth: 2, //largura do tab
+  }
